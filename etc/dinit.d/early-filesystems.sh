d4120a74e4f0b57e3528423ffa1b81f0aacf036f
dkwo <npiazza@disroot.org>
2026-06-15 16:50:18 +0800

    earlyfs: use conditional just in case

--- etc/dinit.d/early-filesystems.sh
+++ etc/dinit.d/early-filesystems.sh
@@ -6,13 +6,14 @@ if [ "$1" = start ]; then
 
     PATH=/usr/bin:/usr/sbin:/bin:/sbin
 
-    # todo: mount cgroup v2
+    # todo: cgroup v2
 
-    # /run is already mounted by the initramfs
-    # mount -n -t tmpfs -o mode=775 tmpfs /run
-    # mountpoint -q /run || mount -o mode=0755,nosuid,nodev -t tmpfs run /run
-    # mkdir /run/lock /run/udev
-    # "hidepid=1" not sure
-    # mount -n -t proc -o remount,hidepid=1 proc /proc
+    mountpoint -q /proc || mount -o nosuid,noexec,nodev -t proc proc /proc
+    mountpoint -q /sys || mount -o nosuid,noexec,nodev -t sysfs sys /sys
+    mountpoint -q /run || mount -o mode=0755,nosuid,nodev -t tmpfs run /run
+    mountpoint -q /dev || mount -o mode=0755,nosuid -t devtmpfs dev /dev
+    mountpoint -q /dev/pts || mount -o mode=0620,gid=5,nosuid,noexec -n -t devpts devpts /dev/pts
+    mountpoint -q /dev/shm || mount -o mode=1777,nosuid,nodev -n -t tmpfs shm /dev/shm
+    mountpoint -q /sys/kernel/security || mount -n -t securityfs securityfs /sys/kernel/security
 
 fi
